56621c7df57632138c9d05baace2e8c52e906baf
Iaroslava <Iaroslava@MacBook-Pro.local>
2024-04-01 18:40:55 -0500

    Products fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
               element={<Login />}
             />
             <Route
-              path="/books"
+              path="/products"
               element={<AllProducts />}
             />
             <Route
--- client/src/components/AllProducts.jsx
+++ client/src/components/AllProducts.jsx
@@ -1,3 +1,32 @@
-export default function  AllProducts() {
-    
+import { useState, useEffect } from "react";
+
+export default function AllProducts() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch("/api/products");
+      const json = await response.json();
+      setProducts(json);
+    };
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p>{product.price}</p>
+              <button>View Product</button>
+              <button>Add Product</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }

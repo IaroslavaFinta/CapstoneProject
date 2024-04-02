27def090472b46aaa4845c9b412ca03251285a40
Iaroslava <Iaroslava@MacBook-Pro.local>
2024-04-01 20:37:10 -0500

    Products search

--- client/src/components/AllProducts.jsx
+++ client/src/components/AllProducts.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 export default function AllProducts() {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     const getProducts = async () => {
@@ -15,22 +16,42 @@ export default function AllProducts() {
     getProducts();
   }, []);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
   return (
     <>
+      <div className="search-bar">
+        <div className="input-wrapper">
+          <input
+            className="search-input"
+            type="search"
+            placeholder="Type to search for a product"
+            value={searchInput}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
       <ul className="product">
-        {products.map((product) => {
-          return (
-            <li key={product.id} className="product">
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>{product.price}</p>
-              <button onClick={() => navigate(`/products/${product.id}`)}>
-                      View Product
-              </button>
-              <button>Add Product</button>
-            </li>
-          );
-        })}
+        {products
+          .filter((product) =>
+            product.name.toLowerCase().match(searchInput.toLowerCase())
+          )
+          .map((product) => {
+            return (
+              <li key={product.id} className="product">
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+                <button onClick={() => navigate(`/products/${product.id}`)}>
+                  View Product
+                </button>
+                <button>Add Product</button>
+              </li>
+            );
+          })}
       </ul>
     </>
   );

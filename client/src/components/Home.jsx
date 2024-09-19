2581e6790a5b60ff9040801a6acde2bb65a85a0d
Iaroslava <Iaroslava@MacBook-Pro.local>
2024-09-18 20:52:29 -0500

    changed main color

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="home">
             <h1>Welcome to Reptile World</h1>
-            <p>At Reptarium, we are passionate about providing our reptiles with everything they need to care for and enjoy their scaly companions.
+            <p>At Reptile World, we are passionate about providing our reptiles with everything they need to care for and enjoy their scaly companions.
                 Step into our store and you'll find a wide selection of supplies necessary to create the perfect habitat for your pet. 
                 Our knowledgeable staff are here to assist you in finding the equipment for your pet and to answer any questions you may have about reptile care.
                 Come visit us today and experience the wonder of the reptile kingdom!</p>
